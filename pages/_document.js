27cabec4c9c9d7390447d39f8c32a1e313e570f4
SANTOSH Pradhan <santosh7pradhan7@gmail.com>
2026-05-14 14:28:25 +0530

    Added Razorpay script to document

--- pages/_document.js
+++ pages/_document.js
@@ -1,14 +1,16 @@
-// pages/_document.js - MINIMAL, STANDARD TEMPLATE
-import { Html, Head, Main, NextScript } from 'next/document';
+import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* ADD THIS LINE BELOW */}
+        <script src="https://checkout.razorpay.com/v1/checkout.js"></script>
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  );
+  )
 }
